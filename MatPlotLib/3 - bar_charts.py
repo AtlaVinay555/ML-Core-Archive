9e6af5bb7f6b55f3c1839e59a91b0b9cfb57c5b8
Atla Vinay <vatla@student.gitam.edu>
2026-07-13 18:57:21 +0530

    Bar Chart label and color comments
    Updated bar chart for improved clarity.

--- MatPlotLib/3 - bar_charts.py
+++ MatPlotLib/3 - bar_charts.py
@@ -7,7 +7,7 @@ x1 = [1,3,5,7,9]
 y1 = [2,4,6,8,10]
 
 
-plt.bar(x,y, label = 'Bars 1', color ='r')
+plt.bar(x,y, label = 'Bars 1', color ='r') # labels for names on graph and colors with color name starting letter
 plt.bar(x1,y1, label = 'Bars 2', color ='c')
 plt.xlabel('X-Axis')
 plt.ylabel('Y-Axis')
